c2d881b839a2a3d962d8d1614ffd1959912d9737
gurunath <gurunath.t@inspireventures.in>
2023-12-09 13:37:35 +0530

    fetching the data from api

--- practice/src/components/ForGuru/Guru.js
+++ practice/src/components/ForGuru/Guru.js
@@ -1,30 +1,46 @@
-import React, { useEffect , useState } from "react";
-const Guru = () =>{
-    const [data , setData] = useState(null);
-    useEffect(()=>{
-        const fetchdata = async () => {
-            try{
-                const response = await fetch("https://dummyjson.com/products");
-                const reasult = response.json();
-                setData(reasult)
-            }
-            catch{
+import React, { useEffect, useState } from "react";
+const Guru = () => {
+    const [Data, setData] = useState([]);
+  
+    useEffect(() => {
+        fetch("https://dummyjson.com/products")
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setData(data.products);
+            });
+    }, []);
 
-            }
-        }
-        fetchdata();
-    },[])
     return (
-        <div>
-            <h1>Guru Practice</h1>
-            {
-             data.map((item)=>{
-               return(
-                console.log(item.products)
-               )
-             })
-            }
+        <div className="container">
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>TITLE</th>
+                        <th>DESCRIPTION</th>
+                        <th>PRICE</th>
+                        <th>PHOTO</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        Data.map((productDetail)=>{
+                            return(
+                                <tr>
+                                <td>{productDetail.id}</td>
+                                <td>{productDetail.title}</td>
+                                <td>{productDetail.description}</td>
+                                <td>{productDetail.price}</td>
+                                <td><img className="img_width" src={productDetail.thumbnail}></img></td>
+                            </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
         </div>
-      );
+    );
 }
 export default Guru;
